7c86aea64d7acec9562bd003dc6e7733e64607f8
Tomas Doran <tdoran@yelp.com>
2015-04-06 17:19:46 +0100

    Make the ELB ask for a specific status page

--- elb/main.tf
+++ elb/main.tf
@@ -16,7 +16,7 @@ resource "aws_elb" "mesos" {
     healthy_threshold = 2
     unhealthy_threshold = 2
     timeout = 3
-    target = "HTTP:80/"
+    target = "HTTP:80/status"
     interval = 30
   }
 
