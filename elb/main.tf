cb600f05584a14a7d3cfa7a6ced229cba7a5b801
Tomas Doran <tdoran@yelp.com>
2015-04-06 17:07:03 +0100

    Revert "Comment out"
    This reverts commit b2ae30933ea6f9aa0bf70e6868f0c8aaf6b38dd6.

--- elb/main.tf
+++ elb/main.tf
@@ -19,7 +19,7 @@ resource "aws_elb" "mesos" {
     interval = 30
   }
 
-  instances = [] #"${split(\",\", var.instance_ids)}"]
+  instances = ["${split(\",\", var.instance_ids)}"]
   cross_zone_load_balancing = true
 }
 
