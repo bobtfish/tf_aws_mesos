989aa6b62685e3275caa43c853a832607336d1dd
Tomas Doran <tdoran@yelp.com>
2015-04-06 16:55:12 +0100

    Experiment with an ELB

--- mesos_master/output.tf
+++ mesos_master/output.tf
@@ -1,3 +1,7 @@
 output "public_ips" {
     value = "${join(\",\", aws_instance.mesos_master.*.public_ip)}"
 }
+output "instance_ids" {
+    value = "${join(\",\", aws_instance.mesos_master.*.id)}"
+}
+
