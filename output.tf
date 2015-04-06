383bdb6c85b69f39ece82a1243b55a4207d490a8
Tomas Doran <tdoran@yelp.com>
2015-04-06 16:30:31 +0100

    No trailing slash

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "master_public_ips" {
   value = "${module.mesos_master.public_ips}"
 }
 output "marathon_api" {
-  value = "http://marathon.admin.${var.domain}/"
+  value = "http://marathon.admin.${var.domain}"
 }
 
