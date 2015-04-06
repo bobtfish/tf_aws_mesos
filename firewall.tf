f2ee6790a56dd174134c2f1a1caea9b671ade729
Tomas Doran <tdoran@yelp.com>
2015-04-06 21:15:53 +0100

    Remove https rule we don't use

--- firewall.tf
+++ firewall.tf
@@ -30,22 +30,6 @@ resource "aws_security_group" "mesos_http" {
   }
 }
 
-resource "aws_security_group" "mesos_https" {
-  name = "mesos_https"
-  description = "Allow all inbound HTTPS traffic"
-  vpc_id = "${var.vpc_id}"
-  ingress {
-      from_port = 443
-      to_port = 443
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name = "mesos_https"
-  }
-}
-
 resource "aws_security_group" "mesos_ssh" {
   name = "mesos_ssh"
   description = "Allow all inbound SSH traffic"
