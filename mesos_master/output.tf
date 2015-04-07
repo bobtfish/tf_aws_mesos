4315c7de3237d02518f56666774d9f2e16cc511a
Tomas Doran <tdoran@yelp.com>
2015-04-07 05:02:04 +0100

    These outputs can't be found any more

--- mesos_master/output.tf
+++
@@ -1,7 +0,0 @@
-output "public_ips" {
-    value = "${join(\",\", aws_instance.mesos_master.*.public_ip)}"
-}
-output "instance_ids" {
-    value = "${join(\",\", aws_instance.mesos_master.*.id)}"
-}
-
